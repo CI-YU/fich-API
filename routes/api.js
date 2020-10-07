eb16c4737a6771f77c421e6aa409b177739f908c
CI-YU <a1245bill@yahoo.com.tw>
2020-10-07 21:49:30 +0800

    新增api路由

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,9 @@
-const express = require("express");
-const router  = express.Router();
+var express = require('express');
+var router = express.Router();
 
-router.post("/",function(req,res,next){
-    res.send("123");
-  
+/* GET users listing. */
+router.post('/', function(req, res, next) {
+  res.send('respond with a resource');
 });
 
-module.exports= router;
+module.exports = router;
